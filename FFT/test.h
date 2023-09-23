da1a84f86d903cd4db7543e6c046d5d324121008
Yuyu378 <70588826+Yuyu378@users.noreply.github.com>
2023-09-23 15:21:25 +0800

    Update test.h

--- FFT/test.h
+++ FFT/test.h
@@ -17,12 +17,12 @@
 //			~ fix memory leak when used in recursion functions
 //		* Rewrite CooleyTukey_Radix2_nonRecursion function
 //			~ avoid memory leak when used in recursion functions
-//		* Add Radar function to test Radar's Algorithm
+//		* Add Rader function to test Rader's Algorithm
 //		* Add CooleyTukey_HybridRadix function to test Variation Cooley-Tukey Algorithm
 //		* Add Bluestein function to test Bluestein's Algorithm
 //		* Rewrite Compare function
 //			~ Modularize the execution of the FFT transformation into a function named test
-//			~ Add the comparison functions of Radar's/Variation Cooley-Tukey/Bluestein's algorithm
+//			~ Add the comparison functions of Rader's/Variation Cooley-Tukey/Bluestein's algorithm
 // 
 
 #pragma once
@@ -39,7 +39,7 @@ dcomplex* CooleyTukey_Radix2_Recursion_8pointAccelerate(dcomplex* data, unsigned
 
 dcomplex* CooleyTukey_Radix2_nonRecursion(dcomplex* data, unsigned N, bool is_forward, bool keep_input);
 
-dcomplex* Radar(dcomplex* data, unsigned N, bool is_forward, bool keep_input, dcomplex* (*fft)());
+dcomplex* Rader(dcomplex* data, unsigned N, bool is_forward, bool keep_input, dcomplex* (*fft)());
 
 dcomplex* CooleyTukey_HybridRadix(dcomplex* data, unsigned N, bool is_forward, bool keep_input, dcomplex* (*fft)());
 
